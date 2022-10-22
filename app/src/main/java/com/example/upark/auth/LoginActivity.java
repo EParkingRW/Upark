259ebd42df65203adce40116b9b954b93f6efd97
ndatumuremyi <paternenught@gmail.com>
2022-10-22 19:53:21 +0200

    adding login page with let you in

--- app/src/main/java/com/example/upark/auth/LoginActivity.java
+++ app/src/main/java/com/example/upark/auth/LoginActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.upark.R;
 
-public class Login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
     }
 }
