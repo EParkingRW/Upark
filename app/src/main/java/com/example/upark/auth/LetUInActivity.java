cce7a7c74b9460570bfa2bd89c96c259b06842a0
ndatumuremyi <paternenught@gmail.com>
2022-10-24 13:58:45 +0200

    adding the bottom menu

--- app/src/main/java/com/example/upark/auth/LetUInActivity.java
+++ app/src/main/java/com/example/upark/auth/LetUInActivity.java
@@ -2,15 +2,43 @@ package com.example.upark.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.upark.R;
 
 public class LetUInActivity extends AppCompatActivity {
 
+    private Button sign_in;
+    private Button sign_up;
+    private Button sign_with_google;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        sign_in = findViewById(R.id.login_btn);
+        sign_up = findViewById(R.id.sign_up_btn);
+        sign_with_google = findViewById(R.id.continue_with_google);
+
+        addListeners();
+    }
+
+    private void addListeners() {
+        try {
+            final Intent[] signInIntent = {null};
+            sign_in.setOnClickListener(view -> {
+                if(signInIntent[0] == null){
+                    signInIntent[0] = new Intent(this, LoginActivity.class);
+                }
+                this.startActivity(signInIntent[0]);
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/upark/auth/LoginActivity.java
+++ app/src/main/java/com/example/upark/auth/LoginActivity.java
@@ -2,15 +2,37 @@ package com.example.upark.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.example.upark.MainActivity;
+import com.example.upark.MapsActivity;
 import com.example.upark.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Button login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_btn = findViewById(R.id.login_btn);
+
+        addListeners();
+    }
+
+    private void addListeners() {
+        try {
+            final Intent[] loginIntent = {null};
+            login_btn.setOnClickListener(view -> {
+                if(loginIntent[0] == null){
+                    loginIntent[0] = new Intent(this, MainActivity.class);
+                }
+                this.startActivity(loginIntent[0]);
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
