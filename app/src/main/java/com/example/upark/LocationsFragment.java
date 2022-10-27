01e841fe52735be8486afed654d99fda4fc65056
ndatumuremyi <paternenught@gmail.com>
2022-10-27 09:13:19 +0200

    adding popup dialog

--- app/src/main/java/com/example/upark/LocationsFragment.java
+++ app/src/main/java/com/example/upark/LocationsFragment.java
@@ -1,16 +1,24 @@
 package com.example.upark;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresPermission;
@@ -28,6 +36,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
@@ -40,6 +49,7 @@ public class LocationsFragment extends Fragment implements OnMapReadyCallback {
 
     // TODO: Rename and change types of parameters
     private GoogleMap mMap;
+    Marker myMarker;
 
     public LocationsFragment() {
         // Required empty public constructor
@@ -122,14 +132,36 @@ public class LocationsFragment extends Fragment implements OnMapReadyCallback {
 
                         Log.d("GPSTag", "the use location gotten ");
                         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney")
+                        MarkerOptions markerOptions = new MarkerOptions().position(sydney).title("Marker in Sydney")
                                 // below line is use to add custom marker on our map.
-                                .icon(BitmapFromVector(this.requireContext(), R.drawable.ic_baseline_local_parking_24)));
-
+                                .icon(BitmapFromVector(this.requireContext(), R.drawable.ic_baseline_local_parking_24));
+                        myMarker = mMap.addMarker(markerOptions);
+                        mMap.setOnMarkerClickListener(marker -> {
+                            if(marker.equals(myMarker)){
+                                showDialog();
+                            }
+                            return false;
+                        });
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
                         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,10));
                     }
                 });
+
+    }
+    private void showDialog() {
+
+        final Dialog dialog = new Dialog(this.requireActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.details_dialog);
+
+        Button close_btn = dialog.findViewById(R.id.close_btn);
+        close_btn.setOnClickListener(listener -> dialog.dismiss());
+        Button showDetails = dialog.findViewById(R.id.details_btn);
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
 
     }
 }
